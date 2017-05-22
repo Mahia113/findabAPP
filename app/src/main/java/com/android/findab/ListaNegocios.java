b7667d6e20019e39204d77de304eaff9a325ba01
José Luis López A <joseluiss.113@gmail.com>
2017-05-22 00:24:32 -0500

    Lista negocios terminada. Faltan mas detalles. Cambios.

--- app/src/main/java/com/android/findab/ListaNegocios.java
+++ app/src/main/java/com/android/findab/ListaNegocios.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -73,6 +74,13 @@ public class ListaNegocios extends AppCompatActivity
         inicializarDatos();
 
         inicializaAdaptador();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_negocios, menu);
+        return true;
     }
 
     public void inicializarDatos()
